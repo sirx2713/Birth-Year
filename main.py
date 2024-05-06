f7080814879ac83a1e3701d4e66d7575fb5c2947
Mabende <belindamabende@gmail.com>
2024-05-06 18:00:30 +0200

    Back to the snake!

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+print(type(name))
+birth_year = input("Enter the year you were born: ")
+print(type(birth_year))
+age = 2023 - int(birth_year)
+print(age)
+print(type(age))
